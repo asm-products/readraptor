642a4870d29ffc101c3d7a315f0cebb50ab0e7c5
Dave Newman <dave@snappyco.de>
2014-02-22 15:17:05 -0800

    fix non-uniqueness in migration

--- db/migrations/20140221150028_CreateUserCallbacks.sql
+++ db/migrations/20140221150028_CreateUserCallbacks.sql
@@ -5,9 +5,9 @@ CREATE TABLE user_callbacks (
   reader_id       int       NOT NULL,
   created_at      timestamp NOT NULL,
 
-  CONSTRAINT fk_expected_readers_content_items FOREIGN KEY (content_item_id) REFERENCES content_items (id),
-  CONSTRAINT fk_expected_readers_readers FOREIGN KEY (reader_id) REFERENCES readers (id),
-  CONSTRAINT uq_expected_readers_content_item_reader UNIQUE(content_item_id, reader_id)
+  CONSTRAINT fk_user_callbacks_content_items FOREIGN KEY (content_item_id) REFERENCES content_items (id),
+  CONSTRAINT fk_user_callbacks_readers FOREIGN KEY (reader_id) REFERENCES readers (id),
+  CONSTRAINT uq_user_callbacks_content_item_reader UNIQUE(content_item_id, reader_id)
 );
 
 -- +goose Down
